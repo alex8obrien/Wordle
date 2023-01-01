6d17f0c1e29707e553ae13b16158ecf0ccd48714
Alex O'Brien <alex8obrien@gmail.com>
2023-01-01 23:37:35 +0000

    Added Introduction

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -1,11 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Lib;
 
-namespace Wordle;
-
-internal static class Program
+namespace Wordle
 {
-    public static void Main(string[] args)
+    internal static class Program
     {
-        Console.WriteLine("Hello, World!");
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Introduction();
+            
+        }
+        
+        private static void Introduction()
+        {
+            Console.WriteLine("Hello, welcome to the game of Wordle!");
+            bool yn = StdInp.InputYNAsBool("Would you like to read the rule for Wordle?");
+
+            if (!yn) return;
+            
+            Console.WriteLine("The rules are simple, you have 6 chances to guess the correct word.");
+            Console.WriteLine("If you guess the word correctly, you will get 1 point.");
+            Console.WriteLine("If you guess the word incorrectly, your score remains the same.");
+            Console.WriteLine("If you run out of chances, you will lose 1 point.");
+            Console.WriteLine("When you want to finish, type 'exit' to exit the game and see your score.");
+        }
     }
 }
