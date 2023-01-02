6b2e264b5bdc2a23f1aa6c8ace407a93581f53db
Alex O'Brien <alex8obrien@gmail.com>
2023-01-02 12:11:15 +0000

    Added game loop

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -6,9 +6,25 @@ namespace Wordle
     {
         public static void Main(string[] args)
         {
+            bool gameOver = false;
+            int score = 0;
+            
             Console.WriteLine("Hello, World!");
             Introduction();
-            string secretWord = GetSecretWord();
+
+            do
+            {
+                string secretWord = GetSecretWord();
+                score += PlayGame();
+                
+                bool playAgain = StdInp.InputYNAsBool("Do you want to play again?");
+                if (!playAgain)
+                { gameOver = true; }
+                
+            } while (!gameOver);
+            
+            Console.WriteLine("Thanks for playing!");
+            Console.WriteLine($"Your final score is {score}");
         }
 
         private static void Introduction()
@@ -20,7 +36,6 @@ namespace Wordle
 
             Console.WriteLine("The rules are simple, you have 6 chances to guess the correct word.");
             Console.WriteLine("If you guess the word correctly, you will get 1 point.");
-            Console.WriteLine("If you guess the word incorrectly, your score remains the same.");
             Console.WriteLine("If you run out of chances, you will lose 1 point.");
             Console.WriteLine("When you want to finish, type 'exit' to exit the game and see your score.");
         }
@@ -34,6 +49,11 @@ namespace Wordle
             int randomLine = randInt.Next(1, numberOfLines);
 
             return FileIO.LineAt(filePath, randomLine);
+        }
+
+        private static int PlayGame()
+        {
+            return -1;
         }
     }
 }
